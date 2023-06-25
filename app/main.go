07f6360b7e599c62cec9c7a6e03227edd35c78db
ponta10 <mponta2002@gmail.com>
2023-06-25 12:46:54 +0900

    create main.go

---
+++ app/main.go
@@ -0,0 +1,27 @@
+// ./main.go
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/jinzhu/gorm"
+    "gin_auth/app/controllers"
+    "gin_auth/app/models"
+)
+
+func main() {
+    db, err := gorm.Open("postgres", "host=localhost user=gorm dbname=gorm password=gorm sslmode=disable")
+    if err != nil {
+        panic("failed to connect database")
+    }
+    defer db.Close()
+
+    db.AutoMigrate(&models.User{}, &models.Post{})
+
+    r := gin.Default()
+
+    r.POST("/register", func(c *gin.Context) { controllers.RegisterEndpoint(c, db) })
+    r.POST("/login", func(c *gin.Context) { controllers.LoginEndpoint(c, db) })
+
+    r.Run(":8080")
+}
+
