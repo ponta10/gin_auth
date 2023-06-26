ad3101b5bbffc2f24cc1efd961a8babfeb6a5bf0
ponta10 <mponta2002@gmail.com>
2023-06-26 20:56:53 +0900

    add comment post

--- app/controllers/postController.go
+++ app/controllers/postController.go
@@ -9,19 +9,24 @@ import (
 )
 
 func CreatePostEndpoint(c *gin.Context, db *gorm.DB) {
+    // c.Get("userId")を用いてリクエストのcontextからuserIdを取得します。このuserIdは、前段階のミドルウェアであるAuthMiddleware()によって設定されたもので、現在認証されているユーザーのIDを表します。
     userId, exists := c.Get("userId")
     if !exists {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "User ID not found in context"})
         return
     }
     var post models.Post
+
+    // c.ShouldBindJSON(&post)を用いてリクエストボディのJSONをPost型のオブジェクトpostにバインドします。
     if err := c.ShouldBindJSON(&post); err != nil {
         c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
     }
 
+    // postのUserIDフィールドに、contextから取得したuserIdをセットします。
     post.UserID = userId.(uint)
 
+    // models.CreatePost(db, &post)を用いてpostをデータベースに保存します。
     if err := models.CreatePost(db, &post); err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating post"})
         return
@@ -31,12 +36,14 @@ func CreatePostEndpoint(c *gin.Context, db *gorm.DB) {
 }
 
 func GetUserPosts(c *gin.Context, db *gorm.DB) {
+    // c.Get("userId")を用いてリクエストのcontextからuserIdを取得します。
     userId, exists := c.Get("userId")
     if !exists {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "User ID not found in context"})
         return
     }
 
+    // 証されたユーザー（userId）の投稿を全て取得します。
     user, err := models.GetUserWithPosts(db, userId.(uint))
     if err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error retrieving user posts"})
--- app/models/post.go
+++ app/models/post.go
@@ -12,12 +12,15 @@ type Post struct {
     UserID uint `json:"user_id"`
 }
 
+// db.Create(post) の部分で新しい投稿のレコードをデータベースに保存しています
 func CreatePost(db *gorm.DB, post *Post) error {
 	return db.Create(post).Error
 }
 
 func GetUserWithPosts(db *gorm.DB, userID uint) (*User, error) {
     var user User
+    // reloadメソッドはGORMのEager Loadingを実現するためのメソッドで、この場合"Posts"という引数はUserモデルにおける関連付けられたPostモデルをEager Loadingすることを指示しています。
+    // つまり、ユーザー情報を取得する際に同時にそのユーザーの投稿も取得します。
     if err := db.Preload("Posts").First(&user, userID).Error; err != nil {
         return nil, err
     }
