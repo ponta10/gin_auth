d7bb0a278756eed5210185df8f927890935b7add
ponta10 <mponta2002@gmail.com>
2023-06-26 19:14:43 +0900

    add comment auth

--- app/controllers/authController.go
+++ app/controllers/authController.go
@@ -2,7 +2,6 @@
 package controllers
 
 import (
-	"fmt"
 	"gin_auth/models"
 	"net/http"
 
@@ -12,27 +11,30 @@ import (
 
 func RegisterEndpoint(c *gin.Context, db *gorm.DB) {
     var user models.User
+    // c.ShouldBindJSON(&user)を用いてクライアントから送られてきたJSONデータをuserオブジェクトにバインド（解析）します
     if err := c.ShouldBindJSON(&user); err != nil {
         c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
     }
 
+    // 入力されたパスワードのハッシュ化
     if err := user.HashPassword(); err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error hashing password"})
         return
     }
 
+    // ユーザーの登録
     if err := models.CreateUser(db, &user); err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating user"})
         return
     }
 
+    // 200番を返す
     c.JSON(http.StatusOK, gin.H{"status": "User created successfully"})
 }
 
 func LoginEndpoint(c *gin.Context, db *gorm.DB) {
     var user, foundUser models.User
-    fmt.Println(user, foundUser)
     if err := c.ShouldBindJSON(&user); err != nil {
         c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
@@ -41,18 +43,19 @@ func LoginEndpoint(c *gin.Context, db *gorm.DB) {
     // user の値を foundUser にコピー
     foundUser = user
 
-    fmt.Println(user, foundUser)
-
+    // 入力された名前と一致するuserの検索
     if err := models.FindUserByUsername(db, &foundUser); err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error finding user"})
         return
     }
 
+    // パスワードのチェック
     if err := foundUser.CheckPassword(user.Password); err != nil {
         c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid password"})
         return
     }
 
+    // tokenの発行
     token, err := foundUser.GenerateToken()
     if err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating token"})
--- app/models/auth.go
+++ app/models/auth.go
@@ -8,8 +8,11 @@ import (
     "time"
 )
 
+// JWTトークンの署名に使用されます。署名により、トークンが改ざんされていないことを確認できます
 var JwtKey = []byte("your_secret_key")
 
+// JWTトークンのペイロード部分に含まれるデータを表すものです
+// jwt.StandardClaimsを埋め込んでおり、これによりJWTの標準的なクレーム（トークンの発行者、被験者、有効期限など）も持つことができます。
 type Claims struct {
     Username string `json:"username"`
     UserID   uint   `json:"user_id"`
@@ -24,30 +27,38 @@ type User struct {
 }
 
 func (user *User) HashPassword() error {
+    // bcryptはハッシュ化のためのライブラリで、安全性の高いハッシュ値を生成します。
+    // パスワードは通常、文字列として保管されますが、bcrypt.GenerateFromPassword関数はバイトスライスを引数に取ります。そのため、user.Password を []byte 型に変換しています
     hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
     if err != nil {
         return err
     }
 
+    // ここでハッシュ化したパスワードをユーザーオブジェクトのパスワードフィールドに代入しています
     user.Password = string(hash)
     return nil
 }
 
 func (user *User) CheckPassword(password string) error {
+    // ハッシュ化したパスワードとテーブルに保存されているパスワードをチェック
     return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 }
 
+// db.Create(user) の部分で新しいユーザーのレコードをデータベースに保存しています。.Error はこの操作中にエラーが発生した場合のエラー情報を取得するためのものです。
 func CreateUser(db *gorm.DB, user *User) error {
     return db.Create(user).Error
 }
 
+// usernameが一致するユーザーを取得
 func FindUserByUsername(db *gorm.DB, user *User) error {
     return db.Where("username = ?", user.Username).First(user).Error
 }
 
 func (user *User) GenerateToken() (string, error) {
+    // トークンの有効期限を設定します。現在時刻から5分後に期限切れとなるようにします。
     expirationTime := time.Now().Add(5 * time.Minute)
 
+    // クレームは、トークンの所有者についての情報（ここではユーザー名とユーザーID）と、このトークンが有効な期間（ここでは上記で設定したexpirationTime）を含むデータ構造
     claims := &Claims{
         Username: user.Username,
         UserID:   user.ID,
@@ -56,6 +67,8 @@ func (user *User) GenerateToken() (string, error) {
         },
     }
 
+    // これらのクレームを持つ新しいJWTを作成します。
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    // token.SignedString(JwtKey)は、JwtKeyを用いてトークンに署名を施し、その署名付きトークンを文字列として返す
     return token.SignedString(JwtKey)
 }
